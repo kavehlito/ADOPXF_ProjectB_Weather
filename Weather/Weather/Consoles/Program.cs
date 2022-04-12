bf693f3954c7657d824ee4ebf9b93fcedb6d8e7b
Kavehlito <kajaliliyan@gmail.com>
2022-04-12 16:12:25 +0200

    removed tasks from console

--- Weather/Weather/Consoles/Program.cs
+++ Weather/Weather/Consoles/Program.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Linq;
-using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Weather.Models;
@@ -72,32 +71,13 @@ namespace Weather.Consoles
             OpenWeatherService service = new OpenWeatherService();
             service.WeatherForecastAvailable += ReportWeatherDataAvailable;
 
-            Task<Forecast> t1 = null, t2 = null, t3 = null, t4 = null;
+            Task<Forecast> t1 = null;
             Exception exception = null;
-            try
-            {
-                double latitude = 59.5086798659495;
-                double longitude = 18.2654625932976;
-                string city = "Märsta";
 
-                //Create the two tasks and wait for comletion
-                await service.GetForecastAsync(city);
-                await service.GetForecastAsync(latitude, longitude);
+            string city = "Märsta";
 
-                t1 = service.GetForecastAsync(city);
-                t2 = service.GetForecastAsync(latitude, longitude);
-                Task.WaitAll(t1, t2);
-
-                t3 = service.GetForecastAsync(city);
-                t4 = service.GetForecastAsync(city);
-                Task.WaitAll(t3, t4);
-                
-            }
-            catch (Exception ex)
-            {
-                //if exception write the message later
-                exception = ex;
-            }
+            //Create the two tasks and wait for comletion
+            await service.GetForecastAsync(city);
 
             theConsole.WriteLine("-----------------");
 
@@ -124,77 +104,7 @@ namespace Weather.Consoles
             theConsoleString.Clear();
             await Task.Delay(1000);
             theConsole.WriteLine("-----------------");
-            
-            if (t2.Status == TaskStatus.RanToCompletion)
-            {
-                Forecast forecast = t2.Result;
-                theConsole.WriteLine($"Weather forecast for {forecast.City}");
-                var GroupedList = forecast.Items.GroupBy(item => item.DateTime.Date);
-                foreach (var group in GroupedList)
-                {
-                    theConsoleString.AppendLine(group.Key.Date.ToShortDateString());
-                    foreach (var item in group)
-                    {
-                        theConsoleString.AppendLine($"   - {item.DateTime.ToShortTimeString()}: {item.Description}, temperature: {item.Temperature} degC, wind: {item.WindSpeed} m/s");
-                    }
-                }
-                theConsole.WriteLine(theConsoleString.ToString());
-            }
-            else
-            {
-                theConsole.WriteLine($"City weather service error");
-                theConsole.WriteLine($"Error: {exception.Message}");
-            }
-            theConsoleString.Clear();
-            theConsole.WriteLine("-----------------");
-            await Task.Delay(1000);
 
-            if (t3?.Status == TaskStatus.RanToCompletion)
-            {
-                Forecast forecast = t3.Result;
-                theConsole.WriteLine($"Weather forecast for {forecast.City}");
-                var GroupedList = forecast.Items.GroupBy(item => item.DateTime.Date);
-                foreach (var group in GroupedList)
-                {
-                    theConsoleString.AppendLine(group.Key.Date.ToShortDateString());
-                    foreach (var item in group)
-                    {
-                        theConsoleString.AppendLine($"   - {item.DateTime.ToShortTimeString()}: {item.Description}, temperature: {item.Temperature} degC, wind: {item.WindSpeed} m/s");
-                    }
-                }
-                theConsole.WriteLine(theConsoleString.ToString());
-            }
-            else
-            {
-                theConsole.WriteLine($"Geolocation weather service error.");
-                theConsole.WriteLine($"Error: {exception.Message}");
-            }
-            theConsoleString.Clear();
-            theConsole.WriteLine("-----------------");
-            await Task.Delay(1000);
-
-            if (t4.Status == TaskStatus.RanToCompletion)
-            {
-                Forecast forecast = t4.Result;
-                theConsole.WriteLine($"Weather forecast for {forecast.City}");
-                var GroupedList = forecast.Items.GroupBy(item => item.DateTime.Date);
-                foreach (var group in GroupedList)
-                {
-                    theConsoleString.AppendLine(group.Key.Date.ToShortDateString());
-                    foreach (var item in group)
-                    {
-                        theConsoleString.AppendLine($"   - {item.DateTime.ToShortTimeString()}: {item.Description}, temperature: {item.Temperature} degC, wind: {item.WindSpeed} m/s");
-                    }
-                }
-                theConsole.WriteLine(theConsoleString.ToString());
-            }
-            else
-            {
-                theConsole.WriteLine($"City weather service error");
-                theConsole.WriteLine($"Error: {exception.Message}");
-            }
-            theConsoleString.Clear();
-            await Task.Delay(1000);
         }
         void ReportWeatherDataAvailable(object sender, string message)
         {
