607efa2d1a6ee0f206d94da6f551ca6ac36c22ba
Kavehlito <kajaliliyan@gmail.com>
2022-04-12 16:17:04 +0200

    bla

--- Weather/Weather/Consoles/Program.cs
+++ Weather/Weather/Consoles/Program.cs
@@ -21,49 +21,6 @@ namespace Weather.Consoles
         }
         #endregion
 
-        /*
-        #region Console Demo program
-        //This is the method you replace with your async method renamed and NON static Main
-        public async Task myMain1()
-        {
-            theConsole.WriteLine("Demo program output");
-
-            //Write an output to the Console
-            theConsole.Write("One ");
-            theConsole.Write("Two ");
-            theConsole.WriteLine("Three and end the line");
-
-            //As theConsole.WriteLine return trips are quite slow in UWP, use instead of myConsoleString to build the the more complex output
-            //string using several myConsoleString.AppendLine instead of several theConsole.WriteLine. 
-            foreach (char c in "Hello World from my Console program")
-            {
-                theConsoleString.Append(c);
-            }
-
-            //Once the string is complete Write it to the Console
-            theConsole.WriteLine(theConsoleString.ToString());
-
-            theConsole.WriteLine("Wait for 2 seconds...");
-            await Task.Delay(2000);
-
-            //Finally, demonstrating getting some data async
-            theConsole.WriteLine("Download from https://dotnet.microsoft.com/...");
-            theConsoleString.Clear();
-            using (var w = new WebClient())
-            {
-                string str = await w.DownloadStringTaskAsync("https://dotnet.microsoft.com/");
-                theConsoleString.Append($"Nr of characters downloaded: {str.Length}");
-            }
-            theConsole.WriteLine(theConsoleString.ToString());
-        }
-
-        //If you have any event handlers, they could be placed here
-        void myEventHandler(object sender, string message)
-        {
-            theConsole.WriteLine($"Event message: {message}"); //theConsole is a Captured Variable, don't use myConsoleString here
-        }
-        #endregion
-        */
 
         public async Task MyMain()
         {
